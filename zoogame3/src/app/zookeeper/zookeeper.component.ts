b94dd066935ffa83c69dd079c695a5ba08941baa
Gust <ody.zafeirakis@gmail.com>
2020-01-14 17:19:51 +0200

    feat(model): add animal and chore models

---
+++ zoogame3/src/app/zookeeper/zookeeper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zookeeper',
+  templateUrl: './zookeeper.component.html',
+  styleUrls: ['./zookeeper.component.css']
+})
+export class ZookeeperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

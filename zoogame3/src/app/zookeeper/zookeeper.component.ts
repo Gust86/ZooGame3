9c2da7a24c4b0ab4189dd43588c442ec0eb56c04
Gust <ody.zafeirakis@gmail.com>
2020-01-14 18:18:14 +0200

    feat(components): Connect child components to parent

--- zoogame3/src/app/zookeeper/zookeeper.component.ts
+++ zoogame3/src/app/zookeeper/zookeeper.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../models/animals';
+import { Chore } from '../models/chores';
+import { animals } from '../list/animal-list';
+import { chores } from '../list/chore-list';
 
 @Component({
   selector: 'app-zookeeper',
@@ -6,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zookeeper.component.css']
 })
 export class ZookeeperComponent implements OnInit {
+  animalList: Animal[] = animals;
+  choreList: Chore[] = chores;
 
   constructor() { }
 
